a2c507e45bfcb4e395fd77d963d1249efe07a7cd
Kyle Goetz <kyle.goetz@stratiumconsulting.com>
2023-05-09 23:09:23 -0500

    line comment

--- grammar/comments.js
+++ grammar/comments.js
@@ -1,17 +1,8 @@
 module.exports = {
-  /**
-   * No preceding whitespace allowed.
-   */
-  comment_line: $ => seq('--', optional(token.immediate(/[^-].*/)), $._eol),
-  
-  // TODO can this appear anywhere in code?
-  // comment_multiline: $ => seq('{-', /.*/m, '-}'),
-  
-  /*
-   * No preceding whitespace, and `---` can have no trailing chars.
-   * Goes until EOF.
-   */
-  comment_fold: $ => seq('---', $._eol, /.+/m),
+  // comment_line: $ => seq('--', optional(token.immediate(/[^-].*/)), /(\r)?\n/),
+
+  // TODO can appear anywhere  
+  comment_multiline: $ => seq('{-', /.*/m, '-}'),
   
   // TODO can this appear anywhere?
   comment_documentation_block: $ => seq('{{', /.*/, '}}'),
