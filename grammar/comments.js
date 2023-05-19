b6d148cd0a6295026ee27dff51eade6f4b39bfa7
Kyle Goetz <kyle.goetz@stratiumconsulting.com>
2023-05-19 01:01:13 -0500

    multiline comments

--- grammar/comments.js
+++ grammar/comments.js
@@ -1,8 +1,9 @@
 module.exports = {
   // comment_line: $ => seq('--', optional(token.immediate(/[^-].*/)), /(\r)?\n/),
 
-  // TODO can appear anywhere  
-  comment_multiline: $ => seq('{-', /.*/m, '-}'),
+  // TODO can appear anywhere 
+  comment_multiline: $ => /{-([\s\S]|.)*?-}/m,
+  // comment_multiline: $ => seq('{-', /.*/m, '-}'),
   
   // TODO can this appear anywhere?
   // comment_documentation_block: $ => seq('{{', /.*/m, '}}'),
