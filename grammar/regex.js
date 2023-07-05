e381da6baca19b0d8b1ffdb091811b43c8d9ca1b
Kyle Goetz <kyle.goetz@stratiumconsulting.com>
2023-07-04 22:27:15 -0500

    fix: #30 - infix ops with path in function application work

--- grammar/regex.js
+++ grammar/regex.js
@@ -2,18 +2,19 @@ const VARID = /[a-zA-Z_\u{1F400}-\u{1FAFF}][a-zA-Z0-9_!'\u{1F400}-\u{1FAFF}]*/u
 
 const SYMBOLY_LIST = '!$%^&*-=+<>.~\\/|:'
 
-const SYMBOLY_ID_START_EQ = new RegExp(`=[${SYMBOLY_LIST}]+`)
-const SYMBOLY_ID_START_PIPE = new RegExp(`|[${SYMBOLY_LIST}]+`)
-const SYMBOLY_ID_NO_START_EQ_OR_PIPE = new RegExp(`[${SYMBOLY_LIST.replace('=','').replace('|','')}][${SYMBOLY_LIST}]+`)
+// `=` and `|` are not operators
+const ONE_SYMBOL = /[$%^&*-+<>.~\\/:]/
 
-// const SYMBOLY_ID_START_EQ = /=[!$%^&*-=+<>.~\\/|:]+/
-// const SYMBOLY_ID_NO_START_EQ = /[!$%^&*-+<>.~\\/|:][!$%^&*-=+<>.~\\/|:]+/
-// const SYMBOLY_ID_START_PIPE = /|[]
+// `||` and `&&` and `->` are not operators
+const TWO_SYMBOL_NO_BOOL = /(-[!$%^&*-=+<.~\\/|:])|(&[!$%^*-=+<>.~\\/|:])|(\|[!$%^&*-=+<>.~\\/:])|[!$%^&*-=+<>.~\\/|:]{2}/
+
+// anything 3+ symbols is an operator
+const THREE_SYMBOL_OR_MORE = /[!$%^&*-=+<>.~\\/|:]{3,}/
 
 module.exports = {
   path: new RegExp(`\\.?(${VARID.source}\\.)+`, 'u'),
   namespace: new RegExp(`\\.?(${VARID.source}\\.)*${VARID.source}`, 'u'),
   varid: VARID,
   lowercase_varid: new RegExp(VARID.source.toLowerCase(), 'u'),
-  symboly_id: new RegExp(`(${SYMBOLY_ID_START_PIPE})|(${SYMBOLY_ID_START_EQ.source})|(${SYMBOLY_ID_NO_START_EQ_OR_PIPE.source})`)
+  symboly_id: new RegExp(`(${ONE_SYMBOL.source})|(${TWO_SYMBOL_NO_BOOL.source})|(${THREE_SYMBOL_OR_MORE.source})`),  
 }
