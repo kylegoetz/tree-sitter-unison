c10bf1241e38a8538b30b22fdb320e396d004de8
Kyle Goetz <kyle.goetz@stratiumconsulting.com>
2024-04-12 11:45:24 -0500

    CI workflow

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -4,7 +4,7 @@
 
 using namespace v8;
 
-extern "C" TSLanguage * tree_sitter_UNISON();
+extern "C" TSLanguage * tree_sitter_unison();
 
 namespace {
 
@@ -17,12 +17,12 @@ void Init(Local<Object> exports, Local<Object> module) {
 
   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_UNISON());
+  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_unison());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("UNISON").ToLocalChecked());
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("unison").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
-NODE_MODULE(tree_sitter_UNISON_binding, Init)
+NODE_MODULE(tree_sitter_unison_binding, Init)
 
 }  // namespace
