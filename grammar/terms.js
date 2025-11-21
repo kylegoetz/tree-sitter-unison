4cd113893037217ac9948fc39fcda75a5eccb93c
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2025-11-21 20:01:42 +0900

    Fix doc blocks before use clauses at top level.

--- grammar/terms.js
+++ grammar/terms.js
@@ -1,5 +1,11 @@
 //alias('([], _) = x', $.DESTRUCTURE)//
-const { sep1, block, open_block_with, openBlockWith, layoutBlock } = require("./util");
+const {
+  sep1,
+  block,
+  open_block_with,
+  openBlockWith,
+  layoutBlock,
+} = require("./util");
 
 module.exports = {
   _statement: ($) =>
@@ -8,37 +14,51 @@ module.exports = {
       $.destructuring_bind, // TODO this is a problem where things gravitate toward this instead of block term for RHS of a KW_EQUALS
       $._block_term,
     ),
-  term_definition2: ($) => prec.right(seq(
-    $._lhs,
-    openBlockWith($, $.kw_equals),
-    $.__block)),
+  term_definition2: ($) =>
+    prec.right(seq($._lhs, openBlockWith($, $.kw_equals), $.__block)),
 
-  __block: $ => seq(
-    optional(seq(sep1($._layout_semicolon, $.use_clause), $._layout_semicolon)),
-    // terminated($, $._statement),
-    seq(prec.right(sep1($._layout_semicolon, choice($._statement))), optional($._layout_semicolon)),
-    $._layout_end
-  ),
-
-  __layout_block: $ => prec.right(seq(
-    optional(seq(sep1($._layout_semicolon, $.use_clause), $._layout_semicolon)),
-    seq(sep1($._layout_semicolon, $._statement), optional($._layout_semicolon)),
-    optional($._layout_end)
-  )),
-
-  binding: ($) =>
+  __block: ($) =>
     seq(
-      optional(seq($.doc_block, $._layout_semicolon)),
-      $._binding,
+      optional(
+        seq(sep1($._layout_semicolon, $.use_clause), $._layout_semicolon),
+      ),
+      // terminated($, $._statement),
+      seq(
+        prec.right(sep1($._layout_semicolon, choice($._statement))),
+        optional($._layout_semicolon),
+      ),
+      $._layout_end,
     ),
-  _binding: $ => seq(
-    optional($.type_signature),
-    alias($.term_definition2, $.term_definition)),
-  destructuring_bind: $ => choice(seq(
-    choice($.parenthesized_or_tuple_pattern),
-    openBlockWith($, $.kw_equals),
-    $.__layout_block,
-  )),
+
+  __layout_block: ($) =>
+    prec.right(
+      seq(
+        optional(
+          seq(sep1($._layout_semicolon, $.use_clause), $._layout_semicolon),
+        ),
+        seq(
+          sep1($._layout_semicolon, $._statement),
+          optional($._layout_semicolon),
+        ),
+        optional($._layout_end),
+      ),
+    ),
+
+  binding: ($) => $._binding,
+  documented_binding: ($) => seq($.doc_block, $._layout_semicolon, $._binding),
+  _binding: ($) =>
+    seq(
+      optional($.type_signature),
+      alias($.term_definition2, $.term_definition),
+    ),
+  destructuring_bind: ($) =>
+    choice(
+      seq(
+        choice($.parenthesized_or_tuple_pattern),
+        openBlockWith($, $.kw_equals),
+        $.__layout_block,
+      ),
+    ),
 
   _block_term: ($) => choice($.literal_function, $._infix_app_or_boolean_op),
   literal_function: ($) => lam($, $._term),
@@ -47,10 +67,12 @@ module.exports = {
   _term2: ($) =>
     choice(alias($.literal_function2, $.literal_function), $._term3),
   _term3: ($) =>
-    prec.right(seq(
-      $._infix_app_or_boolean_op,
-      optional(seq($.type_signature_colon, $._computation_type)),
-    )),
+    prec.right(
+      seq(
+        $._infix_app_or_boolean_op,
+        optional(seq($.type_signature_colon, $._computation_type)),
+      ),
+    ),
   _term4: ($) => repeat1($._term_leaf),
 
   _infix_app_or_boolean_op: ($) =>
@@ -60,4 +82,4 @@ module.exports = {
 };
 
 lam = ($, term) =>
-  seq($._prefix_definition_name, alias('->', $.arrow_symbol), term)
+  seq($._prefix_definition_name, alias("->", $.arrow_symbol), term);
